dee249b64945e68bd9b90732c9d9fdd9c287e06e
eduardo mota <eduardolmota@hotmail.com>
2020-05-06 21:16:07 -0300

    Adicionando comentarios aos scripts

--- Assets/Scripts/ProjetoBase.cs
+++ Assets/Scripts/ProjetoBase.cs
@@ -4,36 +4,48 @@ using UnityEngine;
 
 public class ProjetoBase : MonoBehaviour
 {
-    
+    //criando variavel do valor do dinheiro...
     public static float dinheiro = 6000;
-    
+    //criando variaveis da quantidade de itens que estão no "estoque"...
     int quantidaCaC = 7;
     int quantidaAM = 4;
     int quantidaAD = 8;
-
+    //criando função pra comprar algo da categoria de armas corpo a corpo..
     public void ComprarCAC(string name)
     {
+        //se quantidade de armas corpo a corpo for maior que 0...
         if (quantidaCaC > 0)
         {
+            //criando objeto do tipo corpo a corpo...
             CorpoAC corpoAC = new CorpoAC();
+            //trocando o valor do objeto...
             corpoAC.TrocarValor();
+            //se dinheiro for menor que o valor...
             if (dinheiro < corpoAC.Valor)
             {
+                //chamando a função sem dinheiro...
                 UI.singleton.SemDinheiro();
+                //terminando/retornando função...
                 return;
             }
+            //se não... 
             else
             {
+                //diminuindo o valor do dinheiro...
                 dinheiro -= corpoAC.Valor;
+                //trocando nome do objeto...
                 corpoAC.Nome = name;
+                //diminui 1 da quantidade de armas corpo a corpo...
                 quantidaCaC--;
-
+                //chamando a função o que eu comprei...
                 UI.singleton.OqEuComprei(name);
             }
 
         }
+        //se não...
         else
         {
+            //chamando a função sem estoque...
             UI.singleton.SemEstoque();
         }
     }
@@ -92,19 +104,22 @@ public class ProjetoBase : MonoBehaviour
         }
     }
 }
-
+//criando enum para categorizar os itens..
 public enum TipoDeArma
 {
+    //criando as categorias de armas...
     CorpoACorpo, ArmaMagica, ArmaADistancia
 }
-
+//criando classe abstrata...
 public abstract class Arma
 {
+    //criando variaveis para mudar as informações...
     protected TipoDeArma tipodeArma;
     protected string nome;
     protected float valor;
+    //trocando valor do objeto...
     public abstract void TrocarValor();
-
+    //construtor do nome e do valor...
     public string Nome
     {
         get
@@ -116,7 +131,7 @@ public abstract class Arma
             nome = value;
         }
     }
-
+ 
     public float Valor
     {
         get
@@ -129,20 +144,22 @@ public abstract class Arma
         }
     }
 }
-
+//criando classe da categoria de armas corpo a corpo...
 public class CorpoAC : Arma
 {
+    //mostrando tipo da arma (corpo a corpo)...
     public CorpoAC()
     {
         tipodeArma = TipoDeArma.CorpoACorpo;
     }
-
+    //sobrescrevendo a função de trocar o valor...
     public override void TrocarValor()
     {
+        //trocando um valor a arma...
         Valor = 300;
     }
 }
-
+//criando classe da categoria de armas a distancia...
 public class ArmaAD : Arma
 {
     public ArmaAD()
@@ -155,7 +172,7 @@ public class ArmaAD : Arma
         Valor = 650;
     }
 }
-
+//criando classe da categoria de armas magicas...
 public class ArmaMagica : Arma
 {
     public ArmaMagica()
--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -5,43 +5,56 @@ using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
+    //criando componentes de texto...
     public Text dinheiroT;
     public Text semDinheiro;
     public Text oqEuComprei;
     public Text semEstoque;
-
+    //criando um singleton para ser acessado no script anterior...
     public static UI singleton;
-
+    //criando o void start...
     private void Start()
     {
+        //chamando NoDestroy...
         NoDestroy();
+        //chamando a função atualizar dinheiro...
         AtualizarDinherio();
     }
-
+    //criando função pra atualizar a quantia de dinheiro que o player possui...
     public void AtualizarDinherio()
     {
+        //mostrando a quantia restante de dinheiro do player...
         dinheiroT.text = ProjetoBase.dinheiro.ToString() + "$";
     }
-
+    //criando uma função para dizer que o jogador esta sem diheiro...
     public void SemDinheiro()
     {
+        //aparecer texto avisando o player que esta sem dinheiro..
         semDinheiro.enabled = true;
+        //chamando a coroutina...
         StartCoroutine(Timer(semDinheiro));
     }
-
+    //criando função pra dizer o que o player comprou...
     public void OqEuComprei(string name)
     {
+        //chamando função de atualizar dinheiro...
         AtualizarDinherio();
+        //chamando o texto avisando qual item o player comrpou...
         oqEuComprei.enabled = true;
+        //dizendo o que deve aparecer no componente texto...
         oqEuComprei.text = "eu comprei " + name;
+        //chamando a coroutina...
         StartCoroutine(Timer(oqEuComprei));
     }
+    //criando função pra quando o vendedor não possuir produtos suficientes no estoque...
     public void SemEstoque()
     {
+        //ativar o componente texto...
         semEstoque.enabled = true;
+        //chamando a coroutina...
         StartCoroutine(Timer(semEstoque));
     }
-
+    //criando a coroutina para contar o tempo ate sumir os avisos de texto para o player...
     private IEnumerator Timer(Text texto)
     {
         while (true)
@@ -50,7 +63,7 @@ public class UI : MonoBehaviour
             texto.enabled = false;
         }
     }
-
+    //transformando UI em um singleton...
     private void NoDestroy()
     {
         //Faz com que o game object que possui esta classe não seja destruído ao trocar de cena
